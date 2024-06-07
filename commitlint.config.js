48b2278405d850956a0529eacaf359b077763509
MIEZE Matheo <matheo.mieze@etu.univ-littoral.fr>
2024-06-07 14:49:45 +0200

    feat: add commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+export default {
+    extends: ['@commitlint/config-angular'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'chore', 'docs', 'feat', 'fix', 'refactor', 'style', 'test', 'ci', 'build', 'perf', 'revert','breaking-change',
+            ],
+        ],
+    },
+};
